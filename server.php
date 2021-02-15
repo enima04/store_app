e8db897252a5c61315491d3231b6db9306b6438b
amineCHTIOUI <amine.chtioui@yosobox.com>
2021-02-15 11:13:55 +0100

    first commit

---
+++ server.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * ck_14110c64327b5d9959a75f5af4e0f77ffaee6fd2 key
+ * cs_28776e35c0caae564d4396dcad42e6d880d9dd84 secret
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
